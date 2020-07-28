1ae02113cb70573b6b57336555dbbd6bdb51ab4d
geovani-moc <geovani.moc@outlook.com>
2020-07-28 15:52:40 -0300

    finalizado, pronto para os teste

--- selecaoInstrucao.cpp
+++ selecaoInstrucao.cpp
@@ -13,8 +13,7 @@ SelecaoInstrucao::SelecaoInstrucao(RI *representacao_intermediaria)
 void SelecaoInstrucao::imprimir()
 {
     size_t tamanho = instrucoes.size();
-    for (size_t i = 0; i < tamanho; ++i)
-        printf("%s\n", instrucoes[i].c_str());
+    for (size_t i = 0; i < tamanho; ++i) printf("%s\n", instrucoes[i].c_str());
 }
 
 int SelecaoInstrucao::selecionar(RI *raiz)
@@ -23,20 +22,13 @@ int SelecaoInstrucao::selecionar(RI *raiz)
 
     switch (caso)
     {
-    case ADD:
-        return instrucao_ADD(raiz);
-    case SUB:
-        return instrucao_SUB(raiz);
-    case MUL:
-        return instrucao_MUL(raiz);
-    case DIV:
-        return instrucao_DIV(raiz);
-    case LOAD:
-        return instrucao_LOAD(raiz);
-    case STORE:
-        return instrucao_STORE(raiz);
-    case ADDI:
-        return instrucao_ADDI(raiz);
+    case ADD: return instrucao_ADD(raiz);
+    case SUB: return instrucao_SUB(raiz);
+    case MUL: return instrucao_MUL(raiz);
+    case DIV: return instrucao_DIV(raiz);
+    case LOAD: return instrucao_LOAD(raiz);
+    case STORE: return instrucao_STORE(raiz);
+    case ADDI: return instrucao_ADDI(raiz);
     default:
         fprintf(stderr, "Erro: tipo de instrucao nao detectada.\n");
         exit(EXIT_FAILURE);
@@ -46,23 +38,16 @@ int SelecaoInstrucao::selecionar(RI *raiz)
 
 int SelecaoInstrucao::definir_caso(RI *nodo)
 {
-    if (NULL != dynamic_cast<Move_RI *>(nodo))
-        return STORE;
-    else if (NULL != dynamic_cast<Const_RI *>(nodo))
-        return ADDI;
-    else if (NULL != dynamic_cast<Mem_RI *>(nodo))
-        return LOAD;
+    if (NULL != dynamic_cast<Move_RI *>(nodo))return STORE;
+    else if (NULL != dynamic_cast<Const_RI *>(nodo))return ADDI;
+    else if (NULL != dynamic_cast<Mem_RI *>(nodo))return LOAD;
     else if (NULL != dynamic_cast<Binop_RI *>(nodo))
     {
         string temporario = ((Binop_RI *)nodo)->oper;
-        if (temporario.compare("+") == 0)
-            return ADD;
-        else if (temporario.compare("-") == 0)
-            return SUB;
-        else if (temporario.compare("*") == 0)
-            return MUL;
-        else if (temporario.compare("/") == 0)
-            return DIV;
+        if (temporario.compare("+") == 0)return ADD;
+        else if (temporario.compare("-") == 0)return SUB;
+        else if (temporario.compare("*") == 0)return MUL;
+        else if (temporario.compare("/") == 0)return DIV;
     }
 
     return -1;
@@ -150,10 +135,10 @@ int SelecaoInstrucao::instrucao_LOAD(RI *nodo)
 int SelecaoInstrucao::instrucao_STORE(RI *nodo)
 {
     string temporario = "STORE ";
-    Binop_RI *auxiliar = (Binop_RI *)(((Move_RI *)nodo)->esq);
+    Binop_RI *auxiliar = (Binop_RI*)((Mem_RI *)(((Move_RI *)nodo)->esq))->end;
 
-    temporario += ((Temp_RI*) auxiliar->esq)->nome + " ";// erro de saida
-    //temporario += to_string(((Const_RI*) auxiliar->dir)->valor) + " ";//segmentation
+    temporario += ((Temp_RI*) auxiliar->esq)->nome + " ";
+    temporario += to_string(((Const_RI*) auxiliar->dir)->valor) + " ";
     temporario += "r" + to_string(selecionar(((Move_RI *)nodo)->dir));
 
     instrucoes.push_back(temporario);
